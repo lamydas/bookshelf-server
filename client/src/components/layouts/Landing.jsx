6ad7564ad7ce15a9d061985d4c577bd0eefc5541
lamydas.ng <lamydas.ng@gmail.com>
2023-04-18 19:57:24 +0100

    package.json updated to latest3

---
+++ client/src/components/layouts/Landing.jsx
@@ -0,0 +1,27 @@
+
+
+import ImageSlider from './ImageSlider';
+import ClientHolder from './ClientHolder';
+import FeatureBooks from './FeatureBooks';
+import PopularBooks from './PopularBooks';
+import Quotation from './Quotation';
+
+import { SliderData } from './SliderData';
+
+const Landing = () => {
+
+  return (
+    <>
+      
+      <ImageSlider slides={SliderData} />
+      <ClientHolder></ClientHolder>
+      <FeatureBooks></FeatureBooks>
+      <PopularBooks></PopularBooks>
+      <Quotation></Quotation>
+
+
+    </>
+  );
+};
+
+export default Landing;
